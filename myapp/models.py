0edb61cfab45f1a5b109560adfdbf902b748d751
Ajay Singh <ajaysingh.687@rediffmail.com>
2020-01-05 04:10:49 +0530

    Superuser added

--- myapp/models.py
+++ myapp/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Todo(models.Model):
     added_date = models.DateTimeField()
     text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{}'.format(self.text)
